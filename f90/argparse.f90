38c389d4f7cdae32c07c9fe449afd02d8c9d65b2
Rei Matsuzaki <matsuzaki.rei@gmail.com>
2017-11-06 18:40:11 +0900

    add argparser.f90

---
+++ f90/argparse.f90
@@ -0,0 +1,9 @@
+module Mod_ArgParser
+  use Mod_ErrHandle  
+  use Mod_fjson
+  implicit none
+contains
+  subroutine ArgParser_new()
+    write(*,*) "Hello"
+  end subroutine ArgParser_new
+end module Mod_ArgParse
---
+++ py/utest/utest_pwnsh.py
@@ -0,0 +1,10 @@
+import unittest
+class TestPwnsh(unittest.TestCase):
+    def test_first(self):
+        self.assertAlmostEqual(1+1,2)
+
+if __name__ == '__main__':
+    unittest.main()
+
+    
+        
